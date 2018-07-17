5b2aba315342091a38ddc39aa53b04e26549fba4
Anderson Santos <dev.anderson.santos@gmail.com>
2018-07-16 23:58:07 -0300

    Arquivo de configuração

---
+++ instrumentos/config.php
@@ -0,0 +1,17 @@
+<?php
+
+//diretório do sistema
+define("BASE_PATH", dirname(__FILE__)."/");
+define("BASE_URL", "http://instrumentos.test/");
+define("ADM_URL", BASE_URL."painel.php");
+define("CLASSES_PATH", "classes/");
+define("MODULOS_PATH", "modulos/");
+define("CSS_PATH", BASE_URL."css/");
+define("JS_PATH", BASE_URL."js/");
+define("IMG_PATH", BASE_URL."img/");
+
+//banco de dados
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PW", "");
+define("DB_NAME", "instrumentos");
